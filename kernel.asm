c4bf0280087d758df03ecec0ae483d36b84fd082
Ronny Barthmann <32592152+RonnyBarthmann@users.noreply.github.com>
2019-04-05 04:19:25 +0200

    v0.0.2

--- kernel.asm
+++ kernel.asm
@@ -1,6 +1,6 @@
 
-VERSION_STRING_1 equ '[Version: 0.0.1 Beta]'
-VERSION_STRING_2 equ 'v0.0.1 Beta'
+VERSION_STRING_1 equ '[Version: 0.0.2 Beta]'
+VERSION_STRING_2 equ 'v0.0.2 Beta'
 
 InitSegment:
 	MOV AX, CS		; Kopiere das CodeSegment nach AX
@@ -18,37 +18,38 @@ SetupInt21h:
 
 Welcome:
 	CALL ClearScreen
-	MOV AX, BiosHigh
-	CALL SetScreenColor
-	CALL ClearScreen
 	MOV AX, 0
 	MOV BX, 0
+	MOV CX, MSG_Welcome
+	MOV DX, FLG_Welcome
+	CALL WriteRawString
+	MOV AX, 0
+	MOV BX, 7
 	CALL SetCursor
 	CALL GetCursor
-	MOV SI, MSG_Welcome
-	CALL WriteString
 	MOV SI, NextLine
 	CALL WriteString
-	MOV AX, BiosHigh
-	CALL SetScreenColor
+	CALL WriteColorWarning
+	MOV SI, NextLine
+	CALL WriteString
 
 start:
-	MOV AX, BiosHigh
-	CALL SetScreenColor
+	MOV SI, PrefixS
+	CALL WriteString
 	MOV DI, Command
 	CALL ReadString
 	MOV SI, NewLine
 	CALL WriteString
-	MOV CX, 5
-	MOV SI, ExitCmd
-	MOV DI, Command
-	REP CMPSB
-	jz Exit
 	MOV CX, 6
 	MOV SI, ResetCd
 	MOV DI, Command
 	REP CMPSB
 	jz Reset
+	MOV CX, 5
+	MOV SI, ExitCmd
+	MOV DI, Command
+	REP CMPSB
+	jz Exit
 	MOV CX, 4
 	MOV SI, VerComd
 	MOV DI, Command
@@ -69,11 +70,19 @@ start:
 	MOV DI, Command
 	REP CMPSB
 	jz Info
+	MOV CX, 8
+	MOV SI, ChangeC
+	MOV DI, Command
+	REP CMPSB
+	jz Changes
 	MOV CX, 1
 	MOV SI, NoneCmd
 	MOV DI, Command
 	REP CMPSB
 	jz start
+	MOV CX, 20
+	MOV DX, UnknFlg
+	CALL SetFlag
 	MOV SI, Unknown
 	CALL WriteString
 	jmp start
@@ -81,24 +90,24 @@ start:
 Reset:
 	MOV SI, NewLine
 	CALL WriteString
+	MOV CX, 30
+	MOV DX, ResetFg
+	CALL SetFlag
 	MOV SI, ResetSt
 	CALL WriteString
 	MOV SI, NewLine
 	CALL WriteString
-	MOV EAX, 0
-	MOV EBX, 0
-	MOV ECX, 0
-	MOV EDX, 0
-	MOV EDI, 0
-	MOV ESI, 0
-	MOV AX, CS
-	MOV DS, AX
-	MOV ES, AX
-	jmp InitSegment
+	INT 19h
 
 Version:
 	MOV SI, NewLine
 	CALL WriteString
+	CALL WriteColorWarning
+	MOV SI, NewLine
+	CALL WriteString
+	MOV CX, 26
+	MOV DX, VerFlag
+	Call SetFlag
 	MOV SI, VerStrg
 	CALL WriteString
 	MOV SI, NewLine
@@ -107,8 +116,6 @@ Version:
 
 Clear:
 	CALL ClearScreen
-	MOV AX, BiosHigh
-	CALL SetScreenColor
 	MOV AX, 0
 	MOV BX, 0
 	CALL SetCursor
@@ -120,6 +127,9 @@ Help:
 	CALL WriteString
 	MOV SI, VerStrg
 	CALL WriteString
+	MOV SI, NewLine
+	CALL WriteString
+	CALL WriteColorWarning
 	MOV SI, NewLine
 	CALL WriteString
 	MOV SI, HelpStr
@@ -135,7 +145,36 @@ Info:
 	CALL WriteString
 	MOV SI, NewLine
 	CALL WriteString
+	CALL WriteColorWarning
+	MOV SI, NewLine
+	CALL WriteString
 	MOV SI, InfoStr
+	CALL WriteString
+	MOV SI, NewLine
+	CALL WriteString
+	jmp start
+
+Changes:
+	MOV SI, NewLine
+	CALL WriteString
+	MOV SI, VerStrg
+	CALL WriteString
+	MOV SI, NewLine
+	CALL WriteString
+	CALL WriteColorWarning
+	MOV SI, NewLine
+	CALL WriteString
+	MOV SI, Change1
+	CALL WriteString
+	CALL KeyWait
+	CALL ClearScreen
+	MOV AX, 0
+	MOV BX, 0
+	CALL SetCursor
+	CALL WriteColorWarning
+	MOV SI, NewLine
+	CALL WriteString
+	MOV SI, Change2
 	CALL WriteString
 	MOV SI, NewLine
 	CALL WriteString
@@ -144,6 +183,9 @@ Info:
 Exit:
 	MOV SI, NewLine
 	CALL WriteString
+	MOV CX, 28
+	MOV DX, ExitFlg
+	CALL SetFlag
 	MOV SI, ExitStr
 	CALL WriteString
 	MOV SI, NewLine
@@ -162,6 +204,59 @@ Exit:
 	MOV SI, 0
 	CALL ReadString
 	jmp ExitRepeat
+
+SetFlag: ; CX = Leange / DX = Flag
+	PUSH DX
+	PUSH CX
+	MOV AH, 03h
+	MOV BH, 0
+	INT 10h
+	MOV AX, 0B800h
+	MOV ES, AX
+	POP CX
+	MOV AL, DL
+	MOV AH, 0
+	MOV BL, DH
+	MOV BH, 0
+	MOV DI, BX
+	IMUL DI, 160
+	ADD DI, AX
+	POP DX
+	MOV SI, DX
+      SetFlagNext:
+	INC DI
+	MOVSB
+	DEC CX
+	CMP CX, 0
+	JE SetFlagEnd
+	jmp SetFlagNext
+      SetFlagEnd:
+	MOV AX, CS
+	MOV DS, AX
+	MOV ES, AX
+	RETN ; No
+
+SetFlag2: ; AX = X / BX = Y / CX = Leange / DX = Flag
+	PUSH AX
+	MOV AX, 0B800h
+	MOV ES, AX
+	POP AX
+	MOV DI, BX
+	IMUL DI, 160
+	ADD DI, AX
+	MOV SI, DX
+      SetFlag2Next:
+	INC DI
+	MOVSB
+	DEC CX
+	CMP CX, 0
+	JE SetFlag2End
+	jmp SetFlag2Next
+      SetFlag2End:
+	MOV AX, CS
+	MOV DS, AX
+	MOV ES, AX
+	RETN ; No
 
 WriteRawChr: ; AX = X / BX = Y / CX = String
 	PUSHAD
@@ -245,7 +340,7 @@ ClearScreen: ; No
 	MOV CX, ClearFont
 	MOV DX, Color_07h
       ClearNextLine:
-	CALL WriteRawChr
+	CALL WriteRawString
 	INC BX
 	CMP BX, 25
 	JE ClearScreenEnd
@@ -293,6 +388,22 @@ ReadString: ; DI = Buffer
       ReadStringEnd:
 	MOV AL, 0		; Stringende
 	STOSB			; AL speichern kopieren
+	RETN ; No               ; Funktionsrücksprung
+
+KeyWait: ; No
+	MOV CX, 32
+	MOV DX, WaitFlg
+	CALL SetFlag
+	MOV SI, WaitStr
+	CALL WriteString
+	MOV AH, 0		; Funktion
+	INT 16h 		; Zeichen einlesen
+	CMP AL, 13		; Ist AL = 13 ?
+	JE KeyWaitEnd		; dann Abbruch
+	CMP AL, 32		; Ist AL = 32 ?
+	JE KeyWaitEnd		; dann Abbruch
+	JMP KeyWait		; Nächster Durchlauf
+      KeyWaitEnd:
 	RETN ; No               ; Funktionsrücksprung
 
 WriteChr: ; AL = Ascii
@@ -365,6 +476,31 @@ GetCursor: ; No
 	POP DX			; DX Register wiederherstellen
 	RETN ; AX = X / BX = Y  ; Funktionsrücksprung
 
+WriteColorWarning: ; No
+	CALL GetCursor
+	MOV AX, 0
+	CALL SetCursor
+	MOV CX, 51
+	MOV DX, Warnin1
+	PUSHAD
+	CALL SetFlag2
+	POPAD
+	INC BX
+	MOV CX, 60
+	MOV DX, Warnin2
+	PUSHAD
+	CALL SetFlag2
+	POPAD
+	INC BX
+	MOV CX, 71
+	MOV DX, Warnin3
+	PUSHAD
+	CALL SetFlag2
+	POPAD
+	MOV SI, Warning
+	CALL WriteString
+	RETN ; No
+
 Interrupt21h:
 	IRET
 
@@ -375,11 +511,14 @@ Strings:
 			DB ' WinSysOS - Window System Operation System ',VERSION_STRING_1,'                '
 			DB '  (c) WindowSystemCompany 2010 - 2019                                           '
 			DB '                                                                                '
-			DB '--------------------------------------------------------------------------------'
-			DB '                                                                                '
-			DB 'ACHTUNG: WinSysOS ist noch in der Entwicklungsphase',13,10
-			DB '         es wird keinerlei Garantie gegeben ausserdem haftet',13,10
-			DB '         die WinSysCompany weder fuer Daten noch fuer Hardware-Schaeden',13,10,0
+			DB '--------------------------------------------------------------------------------',0
+	FLG_Welcome	DB 80 dup 0Bh
+			DB 80 dup 0Bh
+			DB 80 dup 0Bh
+			DB 80 dup 0Bh
+			DB 80 dup 0Bh
+			DB 80 dup 0Bh
+			DB 80 dup 0Bh,0
 	FullLine	DB '--------------------------------------------------------------------------------',0
 	NextLine	DB 13,10,0
 	Keyboard_DE	DB 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27
@@ -407,6 +546,7 @@ Strings:
 	Color_0Eh	DB 80 dup 0Eh , 0
 	Color_0Fh	DB 80 dup 0Fh , 0
 	Command 	DB 80 dup 00h
+	Buffer		DB 1024 dup 00h
 
 
 
@@ -418,31 +558,93 @@ DataArea:
 	ClsComd db 'cls',0
 	HelpCmd db 'help',0
 	InfoCmd db 'info',0
+	ChangeC db 'changes',0
 	NoneCmd db 0
+	PrefixS db 'CMD> ',0
       StringArea:
 	Unknown db 'Ungueltiger Befehl !',13,10,0
-	ResetSt db 'System wurde zurueckgesetzt',13,10,0
-	ExitStr db 'Bitte PC ausschalten',13,10,0
+	UnknFlg db 0Ch,0Ch,0Ch,0Ch,0Ch,0Ch,0Ch,0Ch,0Ch,0Ch,0Ch,07h,0Ch,0Ch,0Ch,0Ch,0Ch,0Ch,07h,0Fh
+	ResetSt db 'System wird zurueckgesetzt ...',13,10,0
+	ResetFg db 0Fh,0Fh,0Fh,0Fh,0Fh,0Fh,07h,07h,07h,07h,07h,07h,0Ch,0Ch,0Ch,0Ch,0Ch,0Ch,0Ch,0Ch,0Ch,0Ch,0Ch,0Ch,0Ch,0Ch,07h,07h,07h,07h
+	ExitStr db 'Bitte den PC ausschalten ...',13,10,0
+	ExitFlg db 0Fh,0Fh,0Fh,0Fh,0Fh,07h,07h,07h,07h,07h,0Fh,0Fh,07h,0Ch,0Ch,0Ch,0Ch,0Ch,0Ch,0Ch,0Ch,0Ch,0Ch,0Ch,07h,07h,07h,07h
 	VerStrg db 'WindowSystemOS ',VERSION_STRING_2,13,10,0
-	HelpStr db 'ACHTUNG: WinSysOS ist noch in der Entwicklungsphase',13,10
+	VerFlag db 0Bh,0Bh,0Bh,0Bh,0Bh,0Bh,0Bh,0Bh,0Bh,0Bh,0Bh,0Bh,0Bh,0Bh,07h,0Fh,0Fh,07h,07h,07h,07h,07h,0Ch,0Ch,0Ch,0Ch
+	Warning db 'ACHTUNG: WinSysOS ist noch in der Entwicklungsphase',13,10
 		db '         es wird keinerlei Garantie gegeben ausserdem haftet',13,10
-		db '         die WinSysCompany weder fuer Daten noch fuer Hardware-Schaeden',13,10
-		db '',13,10
-		db ' Befehle in WindowSystemOS :',13,10
+		db '         die WinSysCompany weder fuer Daten noch fuer Hardware-Schaeden',13,10,0
+	Warnin1 db 0Ch,0Ch,0Ch,0Ch,0Ch,0Ch,0Ch,0Ch,07h,0Bh,0Bh,0Bh,0Bh,0Bh,0Bh,0Bh,0Bh,07h,07h,07h,07h
+		db 07h,07h,07h,07h,07h,07h,07h,07h,07h,07h,07h,07h,07h,0Ch,0Ch,0Ch,0Ch,0Ch,0Ch,0Ch,0Ch
+		db 0Ch,0Ch,0Ch,0Ch,0Ch,0Ch,0Ch,0Ch,0Ch
+	Warnin2 db 07h,07h,07h,07h,07h,07h,07h,07h,07h,07h,07h,07h,07h,07h,07h,07h,07h,0Ch,0Ch,0Ch,0Ch
+		db 0Ch,0Ch,0Ch,0Ch,0Ch,07h,0Ch,0Ch,0Ch,0Ch,0Ch,0Ch,0Ch,0Ch,07h,07h,07h,07h,07h,07h,07h
+		db 07h,07h,07h,07h,07h,07h,07h,07h,07h,07h,07h,07h,07h,07h,07h,07h,07h,07h
+	Warnin3 db 07h,07h,07h,07h,07h,07h,07h,07h,07h,07h,07h,07h,07h,0Fh,0Fh,0Fh,0Fh,0Fh,0Fh,0Fh,0Fh
+		db 0Fh,0Fh,0Fh,0Fh,0Fh,07h,07h,07h,07h,07h,07h,07h,07h,07h,07h,07h,07h,0Ch,0Ch,0Ch,0Ch
+		db 0Ch,07h,07h,07h,07h,07h,07h,07h,07h,07h,07h,07h,0Ch,0Ch,0Ch,0Ch,0Ch,0Ch,0Ch,0Ch,0Ch
+		db 0Ch,0Ch,0Ch,0Ch,0Ch,0Ch,0Ch,0Ch
+	HelpStr db ' Befehle in WindowSystemOS :',13,10
 		db '',13,10
 		db '  Befehl  | Erklaerung',13,10
 		db '  --------+---------------------------------',13,10
-		db '  reset   | Setzt WinSysOS zurueck',13,10
 		db '  exit    | Faehrt WinSysOS herunter',13,10
 		db '  ver     | Zeigt die Versionsnummer an',13,10
 		db '  help    | Zeigt diese Hilfe an',13,10
 		db '  info    | Zeigt Informationen zu WinSysOS an',13,10
+		db '  changes | Zeigt den Verlauf der Aenderungen an',13,10
 		db '  cls     | Loescht den Bildschirminhalt',13,10
 		db '',13,10
 		db ' Bitte achten sie darauf das ALLE Zeichen kleingeschrieben werden muessen',13,10,0
-	InfoStr db 'Noch keine Informationen vorhanden :-(',13,10,0
+	InfoStr db ' Hallo ich bin der ersteller von WindowSystemOS und auch der gruender',13,10
+		db ' von WindowSystemCompany, wenn ihr euch also fragt wer so ein mist',13,10
+		db ' programmiert, dann bin ich der schuldige :-)',13,10
+		db '',13,10
+		db ' Angefangen hat alles ganz harmlos, ehrlich ;-)',13,10
+		db ' Ich wollte ein Betriebssystem entwickeln das einzig und alleine in',13,10
+		db ' den Bootsektor einer Diskette passt, natuerlich nur mit ein paar',13,10
+		db ' Funktionen, aber als ich dann merkte das das doch nicht geht, weil',13,10
+		db ' 512 Byte einfach zu wenig sind habe ich mich entschlossen ( nach ein',13,10
+		db ' paar Ohrfeigen :-) ) die Grenzen zu loessen und nicht nur einen Sektor zu',13,10
+		db ' nutzen sondern eine Datei !',13,10
+		db ' Ergebnis : Mein OS wird immer nur aus einer Kernel bestehen bis auch das',13,10
+		db ' ABSULUT nicht mehr geht ...',13,10,0
+	Change1 db ' Aenderungen von WindowSystemOS :',13,10
+		db '',13,10
+		db ' * = Geandert / Repariert',13,10
+		db ' ! = Hinweis / Info',13,10
+		db ' + = Hinzugefuegt',13,10
+		db ' - = Entfehrnt',13,10
+		db '',13,10
+		db '  WinSysOS v0.0.1 Beta [05.05.2011] 4365 Byte',13,10
+		db '  ---------------------------------------------',13,10
+		db '  [!] Erste Version',13,10
+		db '  [+] "RESET" zum zurueksetzen',13,10
+		db '  [+] "EXIT" zum runterfahren',13,10
+		db '  [+] "VER" zum herausfinden der OS-Version',13,10
+		db '  [+] "HELP" als kleine Hilfe',13,10
+		db '  [+] "INFO" ein paar Worte vom Programmierer',13,10
+		db '  [+] "CLS" um den Bildschirm zu loeschen',13,10
+		db '  [+] Color unterstuetzung',13,10
+		db '  [+] Back unterstuetzung bei Befehleingabe',13,10
+		db '',13,10,0
+	Change2 db ' Aenderungen von WindowSystemOS :',13,10
+		db '',13,10
+		db ' * = Geandert / Repariert',13,10
+		db ' ! = Hinweis / Info',13,10
+		db ' + = Hinzugefuegt',13,10
+		db ' - = Entfehrnt',13,10
+		db '',13,10
+		db '  WinSysOS v0.0.2 Beta [01.07.2011] 7976 Byte',13,10
+		db '  ---------------------------------------------',13,10
+		db '  [*] "INFO" zeigt jetzt wirklich Text',13,10
+		db '  [*] Komplett neues Style / Farbgebung',13,10
+		db '  [+] "CHANGES" zum verfolgen der Anderungen',13,10
+		db '  [+] Pause funktion',13,10
+		db '  [+] Commander Prefix "CMD>"',13,10,0
 	ChoiceS db 'Bitte Auswealen : ',0
 	BreakSt db 'Abgebrochen...',13,10,0
+	WaitStr db ' Weiter mit ENTER oder SPACE ...',13,10,0
+	WaitFlg db 07h,0Bh,0Bh,0Bh,0Bh,0Bh,0Bh,07h,07h,07h,07h,07h,0Fh,0Fh,0Fh,0Fh,0Fh,07h,07h,07h,07h,07h,07h,0Fh,0Fh,0Fh,0Fh,0Fh,07h,07h,07h,07h
 	NewLine db 13,10,0
 
 Variables:
